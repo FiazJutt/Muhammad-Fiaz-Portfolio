0f33e2f214670caba1c5dcc2c079590c4702d456
Muhammad Fiaz <fiazejaz786@gmail.com>
2025-06-24 17:13:35 +0500

    minor chanes

--- portfolio/flutter.sh
+++ portfolio/flutter.sh
@@ -4,4 +4,5 @@ git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Build the web app
+# flutter build web --release/
 flutter build web --release --base-href /
